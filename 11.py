46f4362da12ef29a655ccab461c192ca01c30382
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-07-19 20:17:15 +0330

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age : "))
+
+if age > 0:
+    if age >= 18:
+        print("True")
+    else:
+        print("False")
+        print("eyval")  # dar sorati ke bozorg tar az 0 va kochak tar 18 bashad
+else:
+    print("age must be larger than zero")
---
+++ 12.py
@@ -0,0 +1,7 @@
+x = int(input("Enter x : "))
+y = int(input("Enter y : "))
+
+if x > y:
+    print(x, 'is bigger')
+else:
+    print(y, 'is bigger')
