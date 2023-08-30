a954363c75a908f475d9fba52ebcc78784ab3956
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-08-30 18:31:31 +0330

    Add files via upload

---
+++ 81.py
@@ -0,0 +1,8 @@
+user = "ali"
+percent = 70
+msg = f"""
+Moshtarak gerami {user.capitalize()} shoma {percent}% az baste
+khod ra masraf kardid.
+"""
+
+print(msg)
---
+++ 82.py
@@ -0,0 +1,4 @@
+x = int(input("Enter integer part : "))
+y = int(input("Enter decimal part : "))
+number = float(f"{x}.{y}")
+print(number)
