2caf159c2d49efa244bc1bd9649ab719b85bd379
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-09-27 11:50:01 +0330

    Add files via upload

---
+++ 85.py
@@ -0,0 +1,9 @@
+# sentence = input("Enter 4 numbers : ")
+# numbers = sentence.split()
+# print(numbers)
+
+# numbers = input("Enter 4 numbers : ").split()
+# print(numbers)
+
+numbers = input("Enter 4 numbers ( separate them with comma ) : ").split(",")
+print(numbers)
